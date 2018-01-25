fb359fc6998bd4ae55ab80f6c50d6323cd40ece3
Knierim <kknierim@usgs.gov>
2018-01-25 13:03:16 -0600

    docs upgraded

--- ZonalAnalysis.py
+++ ZonalAnalysis.py
@@ -1,37 +1,14 @@
 from __future__ import division
 from __future__ import print_function
 '''
-Zonal Statistics
-Vector-Raster Analysis
+Zonal Analysis
 
-Copyright 2013 Matthew Perry
+Developed by Brian Clark, Katherine Knierim, and Leslie Duncan. Portions of 
+this code were modified from Copyright 2013 Matthew Perry, which were 
+licensed under BSD-3 and included in this repo.
 
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice, this
-  list of conditions and the following disclaimer in the documentation and/or
-  other materials provided with the distribution.
-
-* Neither the name of the software nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 import os,sys,cmath
 from osgeo import gdal, ogr, osr
@@ -45,11 +22,20 @@ import matplotlib.pyplot as plt
 import warnings
 gdal.PushErrorHandler('CPLQuietErrorHandler')
  
-"""
-NOTE: The zonal_stats function within this script is the only function that has 
-been corrected in total for handling rotated grids
-"""
+
 def bbox_to_pixel_offsets(gt, bbox, rsize, theta=0, res=1):
+	"""
+	Define raster cells within polygon zone
+
+	Parameters
+	----------
+	gt: geotransform of raster
+	bbox: bounding box from vector (polygon or point buffer)
+	rsize: size of raster (total rows and columns)
+	theta: degree, angle to rotate coordinates
+	res: float, uhhhh Leslie??
+
+"""
 	originX = gt[0]
 	originY = gt[3]
 	pixel_width = gt[1]
@@ -69,8 +55,6 @@ def bbox_to_pixel_offsets(gt, bbox, rsize, theta=0, res=1):
 		y1 = int((env[3] - originY) / -res)
 		y2 = int((env[2] - originY) / -res) + 1
 	else: 
-		# print(bbox)
-		# sys.exit()
 		x1 = int((bbox[0] - originX) / pixel_width)
 		x2 = int((bbox[1] - originX) / pixel_width) + 1
 		y1 = int((bbox[3] - originY) / pixel_height)
@@ -115,22 +99,39 @@ def rotatePt(dX,dY,gt,theta):
  
 
 def zonal_stats(gdb, ras, lyrName=None, fldname=None , 
-	projIn=None, projOut=None, buffDist=500, fact=30, 
+	projIn=None, projOut=None, buffDist=0, fact=30, 
 	nd_thresh=100, filenm='outputfile', csvout=False):
 	"""
-	gdb: str, Filepath of shapefile or geodatabase containing feature class of points or polygons
-	ras: str, Filepath of raster to use for zonal analysis
-	lyrName: str, default None, Name of feature class of points. Default will select first layer in geodatabase
-	fldname: str, default None, Unique identifier field of point locations. Default will select first column in feature class
+	Compute summary statistics of raster cells within vector zone
+
+	Parameters
+	----------
+	gdb: str, Filepath of vector to use for zonal analysis; 
+			shapefile or geodatabase containing feature class, either points 
+			or polygons 
+	ras: str, Filepath of raster to use for zonal analysis;
+		geotif format
+	lyrName: str, default None, Name of feature class of points. Default 
+			will select first layer in geodatabase. Also use default for 
+			shapefiles.
+	fldname: str, default None, Unique identifier field of vector data. 
+			Default will select first column in feature class
 	projIn: proj4 string, default None, Defines projection of vector data
 	projOut: proj4 string, default None, Used to transform the vector data
 			 to match the projection of the raster for correct
 			 intersection
-	buffDist: int, default 500, Linear distance to buffer points, in same units as projOut
-	fact: int, default 4, Ratio of buffer area to raster cell size area. Used to resample the raster to a smaller cell size
-		when vector area is much smaller than raster cell size
-	filenm: str, default 'outputfile', Filepath and name of output file without an extension specified. Default will create a file in the current working directory.
-	csvout: boolean, default False, False = pickled dataframe will be created as output file, True = csv file will be created as output file
+	buffDist: int, default 0, Linear distance to buffer vector data, 
+			in same units as projOut. Default will return the raster cell 
+			value for point data.
+	fact: int, default 30, Ratio of vector area to raster cell size area. 
+			Used to resample the raster to a smaller cell size
+			when vector area is much smaller than raster cell size
+	outND: float, default , No Data value to use as output
+	filenm: str, default 'outputfile', Filepath and name of output file 
+			without an extension specified. Default will create a file in the 
+			current working directory.
+	csvout: boolean, default False, False = pickled dataframe will be created 
+			as output file, True = csv file will be created as output file
 	"""
 	# Open raster
 	rds = gdal.Open(ras, GA_ReadOnly)
@@ -353,24 +354,42 @@ def zonal_stats(gdb, ras, lyrName=None, fldname=None ,
 
 
 def zonal_category(gdb, ras, lyrName=None, fldname=None, projIn=None, 
-				projOut=None, buffDist=500, cmap=None, fact=30, filenm='outputfile',
+				projOut=None, buffDist=0, cmap=None, fact=30, filenm='outputfile',
 				csvout=False):
 	"""
-	gdb: str, Filepath of geodatabase containing feature class of points
-	ras: str, Filepath of raster to use for zonal analysis
-	lyrName: str, default None, Name of feature class of points. Default will select first layer in geodatabase
-	fldname: str, default None, Unique identifier field of point locations. Default will select first column in feature class
+	Compute percent categories of raster cells within vector zone
+
+	Parameters
+	----------
+	gdb: str, Filepath of vector to use for zonal analysis; 
+			shapefile or geodatabase containing feature class, either points 
+			or polygons 
+	ras: str, Filepath of raster to use for zonal analysis;
+		geotif format
+	lyrName: str, default None, Name of feature class of points. Default 
+			will select first layer in geodatabase. Also use default for 
+			shapefiles.
+	fldname: str, default None, Unique identifier field of vector data. 
+			Default will select first column in feature class
 	projIn: proj4 string, default None, Defines projection of vector data
 	projOut: proj4 string, default None, Used to transform the vector data
 			 to match the projection of the raster for correct
 			 intersection
-	buffDist: int, default 500, Linear distance to buffer points, in same units as projOut
-	cmap: dict, default None, Dictionary of raster Values (keys, as int) and category names (values, as str), E.g. {1:'X', 2:'Y'}.
-		Default will create a dictionary using unique raster Values.
-	fact: int, default 4, Ratio of buffer area to raster cell size area. Used to resample the raster to a smaller cell size
-		when vector area is much smaller than raster cell size
-	filenm: str, default 'outputfile', Filepath and name of output file without an extension specified. Default will create a file in the current working directory.
-	csvout: boolean, default False, False = pickled dataframe will be created as output file, True = csv file will be created as output file
+	buffDist: int, default 0, Linear distance to buffer vector data, 
+			in same units as projOut. Default will return the raster cell 
+			value for point data.
+	cmap: dict, default None, Dictionary of raster values (keys, as int) and 
+			category names (values, as str), E.g. {1:'X', 2:'Y'}.
+			Default will create a dictionary using unique raster Values.
+	fact: int, default 30, Ratio of vector area to raster cell size area. 
+			Used to resample the raster to a smaller cell size
+			when vector area is much smaller than raster cell size
+	outND: float, default , No Data value to use as output
+	filenm: str, default 'outputfile', Filepath and name of output file 
+			without an extension specified. Default will create a file in the 
+			current working directory.
+	csvout: boolean, default False, False = pickled dataframe will be created 
+			as output file, True = csv file will be created as output file
 	"""
 	# Open raster
 	rds = gdal.Open(ras, GA_ReadOnly)
@@ -562,3 +581,14 @@ def zonal_category(gdb, ras, lyrName=None, fldname=None, projIn=None,
 		df.to_csv('{}.csv'.format(filenm), index=False)
 	else:
 		df.to_pickle('{}.pkl'.format(filenm))
+
+
+def PckltoRas():
+	"""
+	Convert pickle file of vector zone values to raster 
+
+	Parameters
+	----------
+	"""
+	#read pickle
+	#check for mean? if so, create mean--otherwise category... loop on cmap?
